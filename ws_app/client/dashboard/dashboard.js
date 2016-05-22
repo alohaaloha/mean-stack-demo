9c9d4f9a888f6e94ca9abc885ee7c9f5015f3331
aloha <radomir.aloha@gmail.com>
2016-05-22 20:32:21 +0200

    dashboard started

---
+++ ws_app/client/dashboard/dashboard.js
@@ -0,0 +1,22 @@
+'use strict';
+
+angular.module('wsapp')
+    .config(function ($stateProvider) {
+        $stateProvider
+        .state('dashboard', {
+                url: '/dashboard/{id}',
+                views: {
+                    'content': {
+                        templateUrl: 'dashboard/dashboard.html',
+                        controller: 'DashboardController'
+                    },
+                    'navbar':{
+                        templateUrl: 'navbar/navbar.html',
+                        //controller: 'WelcomeController'
+                    }
+                }
+            })
+
+        ;
+
+    });
