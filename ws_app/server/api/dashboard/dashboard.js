f78ed401eb062693741d557f2ced38df57de2ccd
Nebojsa Popovic <soneneso93@gmail.com>
2016-05-23 04:23:21 +0200

    Backend part for second diagram DONE (tested - WORKING)

--- ws_app/server/api/dashboard/dashboard.js
+++ ws_app/server/api/dashboard/dashboard.js
@@ -10,7 +10,7 @@ var Task = require(__dirname+'/../../../models/task'); // get the mongoose model
 /*Returns list of pair {username: username, numberOfTasks: numberOfTasks}*/ 
 router
 .get('/:id',function(req,res,next){
-    console.log("HADASHDHASHDAHSDHAHSD");
+  
     Project.findOne(
             { _id: req.params.id }
             ,
@@ -59,6 +59,65 @@ router
                     }
                 }
                 res.json({success:true, msg:"USER AND TASKSSSSSSSSSSSS",data:returList});
+        });     
+}).get('/finished/:id',function(req,res,next){  
+  /*Returns list of pair {username: username, numberOfDoneTasks: numberOfDoneTasks} and number of not finished tasks */
+    Project.findOne(
+            { _id: req.params.id }
+            ,
+            function (err, doc) {
+              if (err) {
+                res.send({success:false, msg:'U bazi sjeb'});
+                return;
+              }
+              
+
+            }).populate('usersOnProject').populate('tasks').exec(function(err, entry) {
+            if (err){
+                 res.send({success:false, msg:'Error, couldnt populate with tasks.'});
+                 return;
+            }
+            
+                var neuradjeniZadaci = 0;
+                for(var i =0;i<entry.usersOnProject.length;i++){
+                       entry.usersOnProject[i].brojPojavljivanjaTaskova = 0;
+                       entry.usersOnProject[i].brojUradjenihTaskova = 0;
+                       entry.usersOnProject[i].brojNeuradjenihTaskova = 0;
+                           for(var j = 0;j<entry.usersOnProject[i].tasksImOn.length;j++){
+                                for(var k = 0;k<entry.tasks.length;k++){
+                                      console.log("Entry task")
+                                      console.log(entry.tasks[k]);
+                                      console.log("Project user task")
+                                      console.log(entry.usersOnProject[i].tasksImOn[j]);
+                                      if(entry.tasks[k]._id.equals(entry.usersOnProject[i].tasksImOn[j])){
+                                            entry.usersOnProject[i].brojPojavljivanjaTaskova++;
+                                             console.log("POJAVIO SE!");
+                                             if(entry.tasks[k].status==="DONE"){
+                                                  
+                                                  entry.usersOnProject[i].brojUradjenihTaskova++;   
+                                             }else{
+                                                  entry.usersOnProject[i].brojNeuradjenihTaskova++;
+                                                  neuradjeniZadaci++;
+                                             }
+                                      }
+                                }                    
+                           }
+                }
+                
+                var returList = [];
+                
+                for(var i =0;i<entry.usersOnProject.length;i++){  
+                    
+                    if(entry.usersOnProject[i].brojPojavljivanjaTaskova>0){
+                      
+                      if(entry.usersOnProject[i].brojUradjenihTaskova>0){
+                            returList.push({ "username":entry.usersOnProject[i].username, "numberOfDoneTasks": entry.usersOnProject[i].brojUradjenihTaskova})
+                      }
+                        
+                    }
+                }
+                var returnJSON = {list:returList,"notFinishedTasks":neuradjeniZadaci};
+                res.json({success:true, msg:"USER AND TASKSSSSSSSSSSSS",data:returnJSON});
         });     
 })
 
